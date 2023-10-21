454f42d5e959a18d8c47d30840fe292c8294c8ef
conceptblend <the.concept.blend@gmail.com>
2023-10-21 09:41:26 -0600

    chore(deps): upgrade to latest compatible versions

--- src/sketch.js
+++ src/sketch.js
@@ -1,4 +1,4 @@
-import * as utils from './lib/fx-utils'
+import * as utils from "./lib/fx-utils";
 
 // PARAMETER SETS
 const PARAM_SETS = [
@@ -11,60 +11,65 @@ const PARAM_SETS = [
     duration: 30 * 10, // no unit (frameCount by default; sometimes seconds or frames or whatever)
     exportVideo: false,
     isAnimated: true,
-    renderAsVector: true,
+    renderAsVector: AS_SVG,
   },
 ];
 
 // PARAMETERS IN USE
-const PARAMS = PARAM_SETS[ PARAM_SETS.length - 1 ];
+const PARAMS = PARAM_SETS[PARAM_SETS.length - 1];
 
 // VIDEO
-const EXPORTVIDEO = PARAMS.exportVideo ?? false; // set to `false` to not export
+const EXPORTVIDEO = AS_SVG ? false : PARAMS.exportVideo ?? false; // set to `false` to not export
 const FPS = PARAMS.fps;
 const DURATION = PARAMS.duration;
 
-export const sketch = ( p ) => {
+export const sketch = (p) => {
   let isRecording = false;
-  
+
   p.setup = () => {
     // SVG output is MUCH SLOWER but necessary for the SVG exports
-    p.createCanvas( PARAMS.width, PARAMS.height, PARAMS.renderAsVector ? p.SVG : p.P2D );
-  
-    p.angleMode( p.DEGREES );
-    p.colorMode( p.RGB, 255 );
-    
+    p.createCanvas(
+      PARAMS.width,
+      PARAMS.height,
+      PARAMS.renderAsVector ? p.SVG : p.P2D
+    );
+
+    p.angleMode(p.DEGREES);
+    p.colorMode(p.RGB, 255);
+
     p.noStroke();
-    
+
     // Dependency: Statically added via HTML
-    Math.seedrandom( PARAMS.seed );
-    
-    p.frameRate( FPS );
-  
-    if ( !EXPORTVIDEO && !PARAMS.isAnimated ) p.noLoop();
-  }
+    Math.seedrandom(PARAMS.seed);
+
+    p.frameRate(FPS);
+
+    if (!EXPORTVIDEO && !PARAMS.isAnimated) p.noLoop();
+  };
 
   p.draw = () => {
     p.background(0);
-  
+
     // DO YOUR DRAWING HERE!
     p.noFill();
-    p.stroke( 255, 0, 255 );
-    p.rect( 20 + p.frameCount%60, 20, 30, 30 );
-  
-    if ( EXPORTVIDEO ) {
-      if ( PARAMS.renderAsVector ) throw new Error("Cannot export video when rendering as Vector");
+    p.stroke(255, 0, 255);
+    p.rect(20 + (p.frameCount % 60), 20, 30, 30);
+
+    if (EXPORTVIDEO) {
+      if (PARAMS.renderAsVector)
+        throw new Error("Cannot export video when rendering as Vector");
       if (!isRecording) {
         isRecording = true;
-        console.log('Recording...[ Not implemented ]');
+        console.log("Recording...[ Not implemented ]");
       }
       // Example to end automatically after 361 frames to get a full loop
       if (p.frameCount > DURATION) {
         p.noLoop();
         p.saveConfig();
-        console.log('Done.');
+        console.log("Done.");
       }
     }
-  }
+  };
 };
 
 // %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -72,19 +77,21 @@ export const sketch = ( p ) => {
 
 function getName() {
   // Encode the parameters into the filename
-  return `${PARAMS.name}-${encodeURIComponent(PARAMS.seed)}-${new Date().toISOString()}`;
+  return `${PARAMS.name}-${encodeURIComponent(
+    PARAMS.seed
+  )}-${new Date().toISOString()}`;
 }
 
-function saveImage( ext = 'jpg' ) {
-  save(`${ getName() }.${ ext }`);
+function saveImage(ext = "jpg") {
+  save(`${getName()}.${ext}`);
 }
 
 function saveConfig() {
-  saveJSON( PARAMS, `${getName()}-config.json` );
+  saveJSON(PARAMS, `${getName()}-config.json`);
 }
 
 function downloadOutput() {
-  saveImage( PARAMS.renderAsVector ? 'svg' : 'jpg' );
+  saveImage(PARAMS.renderAsVector ? "svg" : "jpg");
   saveConfig();
 }
 
